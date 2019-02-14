e5909fb53bf9881782edacd66f04f3740142eae0
Keilor <keilorjimenez95@gmail.com>
2019-02-13 22:49:30 -0600

    Se trabaja en la vista de canales

--- app/Http/Controllers/TokenController.php
+++ app/Http/Controllers/TokenController.php
@@ -1,7 +1,9 @@
 <?php
 
 namespace App\Http\Controllers;
+//require_once '/path/to/vendor/autoload.php';
 
+use Twilio\Rest\Client;
 use Illuminate\Http\Request;
 
 class TokenController extends Controller
@@ -29,5 +31,27 @@ class TokenController extends Controller
         );
 
         return response()->json($response);
+    }
+
+    public function index()
+    {
+        $sid    = "AC43ef1f157f97056a8431b1e8dd9b6470";
+        $token  = "5e96d1fda8437ecc7df1839218c20496";
+        $twilio = new Client($sid, $token);
+
+        $channels = $twilio->chat->v2->services("IS8ff2da4c75fe481b87519e39a71ff068")
+                                    ->channels
+                                    ->read();
+
+        foreach ($channels as $record) {
+            $channel = $twilio->chat->v2->services("IS8ff2da4c75fe481b87519e39a71ff068")
+                            ->channels($record->sid)
+                            ->fetch();
+
+            /*print($channel->friendlyName);
+            echo "; ";*/
+        }
+        
+        return view('channels.index', compact('channels','channel'));
     }
 }
--- app/Providers/TwilioAccessTokenProvider.php
+++ app/Providers/TwilioAccessTokenProvider.php
@@ -14,9 +14,9 @@ class TwilioAccessTokenProvider extends ServiceProvider
     {
         $this->app->bind(
             AccessToken::class, function ($app) {
-                $TWILIO_ACCOUNT_SID = config('services.twilio')['accountSid'];
-                $TWILIO_API_KEY = config('services.twilio')['apiKey'];
-                $TWILIO_API_SECRET = config('services.twilio')['apiSecret'];
+                $TWILIO_ACCOUNT_SID = config('services.twilio')['AC43ef1f157f97056a8431b1e8dd9b6470'];
+                $TWILIO_API_KEY = config('services.twilio')['SK535843ae599995582c36f20ce8ba07c6'];
+                $TWILIO_API_SECRET = config('services.twilio')['vJAb0fYtUDsltK69wxveOYm0EbGcXEk2'];
 
                 $token = new AccessToken(
                     $TWILIO_ACCOUNT_SID,
