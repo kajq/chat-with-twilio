9216824598b9cac9781556feea08f17b1e3eac54
Keilor <keilorjimenez95@gmail.com>
2019-02-15 22:28:12 -0600

    Se modifica modelo chanel para retornar lista de canales

--- app/Channel.php
+++ app/Channel.php
@@ -3,8 +3,27 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\http\Request;
+use twilio\Rest\client as Twilio;
+
 
 class Channel extends Model
 {
-    //
+    function __construct()
+    {
+        $this->TWILIO_ACCOUNT_SID   = env('TWILIO_ACCOUNT_SID');
+        $this->TWILIO_TOKEN         = env('TWILIO_TOKEN');
+        $this->TWILIO_SERVICE       = env('TWILIO_SERVICE');
+        $this->TWILIO               = new Twilio($this->TWILIO_ACCOUNT_SID, $this->TWILIO_TOKEN);
+        
+    }
+
+    public function getChannels()
+    {
+        $channels = $this->TWILIO->chat->v2->services($this->TWILIO_SERVICE)
+                                    ->channels
+                                    ->read();
+
+        return $channels;
+    }
 }
